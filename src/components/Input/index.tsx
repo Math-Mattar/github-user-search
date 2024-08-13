e0b7afadc00a9888a202d1c2069df14af2ca493e
Mathews Mattar <mathews.mattar@guidance.dev>
2024-08-13 18:59:41 -0300

    feat/ create input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+import { forwardRef, InputHTMLAttributes } from 'react';
+import { InputVariants } from './variants';
+import { VariantProps } from 'class-variance-authority';
+
+export const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement> & VariantProps<typeof InputVariants>
+  /* eslint-disable react/prop-types */
+>(({ className, variant, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(InputVariants({ variant, className }))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
---
+++ src/components/Input/variants.ts
@@ -0,0 +1,14 @@
+import { cva } from 'class-variance-authority';
+
+export const InputVariants = cva('flex', {
+  variants: {
+    variant: {
+      default: 'bg-red',
+      search:
+        'relative w-full max-w-[730px] border-0 bg-background py-5 pl-20 shadow-card placeholder:text-bio-text',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
