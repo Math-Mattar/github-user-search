5d85701806d66c35356a5b4d927c702ef22ce5f2
Mathews Mattar <mathews.mattar@guidance.dev>
2024-09-02 20:16:21 -0300

    feat: create card container

---
+++ src/containers/Card.tsx
@@ -0,0 +1,13 @@
+import { AccountDetails } from '@/components/AccountDetails.tsx';
+import { PersonalDetails } from '@/components/PersonalDetails.tsx';
+import { JobDetails } from '@/components/JobDetails';
+
+export function Card() {
+  return (
+    <section className="flex w-full flex-col gap-6 rounded-md bg-background-card px-6 pb-12 pt-8 shadow-card dark:bg-background-card">
+      <PersonalDetails />
+      <AccountDetails />
+      <JobDetails />
+    </section>
+  );
+}
