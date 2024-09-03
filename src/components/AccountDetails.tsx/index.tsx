74e5d3b9284040410a6d2c8d99999dd9e2cfd259
Mathews Mattar <mathews.mattar@guidance.dev>
2024-09-03 07:21:02 -0300

    fix: ajust styles account details

--- src/components/AccountDetails.tsx/index.tsx
+++ src/components/AccountDetails.tsx/index.tsx
@@ -1,6 +1,6 @@
 export function AccountDetails() {
   return (
-    <section className="dark:background-details bg-background-details flex w-full justify-center rounded-sm px-4 py-5">
+    <section className="bg-background-details flex w-full justify-center rounded-sm px-4 py-5">
       <div className="w-1/3">
         <h5>Repos</h5>
         <h4>number</h4>
