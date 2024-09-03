18178b3e74877cf00371a38499b352f051255ff2
Mathews Mattar <mathews.mattar@guidance.dev>
2024-09-03 09:43:01 -0300

    feat: add provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Card } from '@/containers/Card';
 import { SearchContainer } from '@/containers/SearchContainer';
+import { UserProvider } from '@/context/UserProvider';
 
 export default function Home() {
   return (
     <main className="flex min-w-80 max-w-[730px] flex-col gap-4">
-      <SearchContainer />
-      <Card />
+      <UserProvider>
+        <SearchContainer />
+        <Card />
+      </UserProvider>
     </main>
   );
 }
