1790eea36a8921ac081349bf5dbc06774eefebf9
Mathews Mattar <mathews.mattar@guidance.dev>
2024-09-03 07:20:18 -0300

    fix: ajust styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} flex flex-col items-center bg-background dark:bg-background`}
+        className={`${inter.className} flex flex-col items-center bg-background`}
       >
         <ThemeProvider
           attribute="class"
