c7dc606e7f73cd670d081b8a36764c2aeff0616e
Mathews Mattar <mathews.mattar@guidance.dev>
2024-09-03 15:56:38 -0300

    fix: ajust get api

--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -1,20 +1,55 @@
+'use client';
+
 import { Button } from '@/components/Button';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Input } from '@/components/Input';
-import { Search } from 'lucide-react';
+
+import { useUser } from '@/hooks/useUser';
+import Search from '@/icons/Search';
 
 export function SearchContainer() {
+  const [username, setUsername] = useState('');
+  const { setUserDetails } = useUser();
+
+  const handleValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  async function fetchUserDetails(username: string) {
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      console.log(res.ok);
+      if (!res.ok) {
+        throw new Error('User not found');
+      }
+      const data = await res.json();
+      setUserDetails(data);
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    fetchUserDetails(username);
+  }
+
   return (
-    <section className="relative">
-      <Search className="absolute left-8 top-[50%] z-10 translate-y-[-50%] stroke-subtitle" />
+    <form action="get" onSubmit={handleSubmit} className="relative">
+      <Search
+        fill="#0079ff"
+        className="absolute left-8 top-[50%] z-10 translate-y-[-50%]"
+      />
       <Input
         type="search"
         variant="search"
         placeholder="Search GitHub username…"
         className="rounded-md"
+        onChange={handleValue}
       />
       <Button type="submit" variant="search" size="search">
         Search
       </Button>
-    </section>
+    </form>
   );
 }
